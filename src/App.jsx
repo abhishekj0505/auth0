04083c088b96ddd7a8d058c411f663e1169e57ef
abhishek jaiswal <a.jaiswal@braininventory.com>
2025-08-29 18:35:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+
+function App() {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isLoading, isAuthenticated } = useAuth0();
+
+  return (
+    <>
+      <h1>Auth0 Implementation</h1>
+      {user ? (
+        <>
+          <div>
+            Hello <span style={{ color: "brown" }}>{user.name}</span>, How are
+            doing?
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            Click the button below to logout.
+          </div>
+          <button
+            onClick={() => {
+              logout();
+            }}
+            style={{ marginTop: "20px" }}
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <div>Login to see magic.</div>
+          <button
+            onClick={() => {
+              loginWithRedirect();
+            }}
+            style={{ marginTop: "20px" }}
+          >
+            Login
+          </button>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
